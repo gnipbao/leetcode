ee6f348ff228783ea9d99b25b851bd728574f106
gnipbao <gnipbao@gmail.com>
2020-05-12 14:46:32 +0800

    Update

--- src/templete/javascript/quickSort.js
+++ src/templete/javascript/quickSort.js
@@ -2,7 +2,7 @@ function quickSort(arr) {
   if (arr.length <= 1) return arr;
   const left = [],
     right = [];
-  const povitIndex = Math.floor(arr.length >> 2);
+  const povitIndex = Math.floor(arr.length >> 1);
   const povit = arr.splice(povitIndex, 1)[0];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < povit) {
