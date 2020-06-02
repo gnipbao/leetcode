473f691d75167722642647ad7c0e72d99e5d334f
gnipbao <gnipbao@gmail.com>
2020-06-02 10:41:58 +0800

    Update

---
+++ src/javascript/3.lengthOfLongestSubstring.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  // 哈希集合，记录每个字符是否出现过
+  const occ = new Set();
+  const n = s.length;
+  // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+  let rk = -1,
+    ans = 0;
+  for (let i = 0; i < n; ++i) {
+    if (i != 0) {
+      // 左指针向右移动一格，移除一个字符
+      occ.delete(s.charAt(i - 1));
+    }
+    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+      occ.add(s.charAt(rk + 1));
+      ++rk;
+    }
+    ans = Math.max(ans, rk - i + 1);
+  }
+  return ans;
+};
